324c251899b9eeedcf66ace512b1b169daf9ac3d
Tatiana Epifanova <t.epifanova@dodopizza.com>
2021-12-06 10:22:43 +0300

    header done

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,10 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".menu-button").addEventListener("click", (e) => {
     document.querySelector(".menu-list").classList.toggle("shown");
     const logo = document.querySelector(".logo");
-    if (!logo.style || logo.style.display == "none") {
-      logo.style.display = "block";
-    } else {
-      logo.style.display = "none";
-    }
   });
 });
